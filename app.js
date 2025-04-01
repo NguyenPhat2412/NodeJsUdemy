ba40258bdc1847c3b95b3c38d973b10f52cd680a
unknown <masterrio2412@gmail.com>
2025-04-01 18:35:37 +0700

    input vs button

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 function rqListener(req, res) {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Second Page</title></head>");
+    res.write(
+      `<body><form action = "/message" method = "POST" ><input type = "text" name = "message" placeholder = "Enter"><button type = "submit">Send</button></form></body>`
+    );
+    res.write("</html>");
+    return res.end(); // Dừng hẳn chương trình sau khi phản hồi
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk); // in ra các chunk dữ liệu
+      body.push(chunk); // lưu trữ các chunk dữ liệu vào mảng body;
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); // nối các chunk lại với nhau
+      const message = parsedBody.split("=")[1]; // tách chuỗi theo dấu "=" và lấy phần tử thứ 2
+      console.log(message); // in ra message
+      fs.writeFileSync("message.txt", message); // ghi vào file message.txt
+      console.log(parsedBody); // in ra cac chunk da noi lai voi nhau
+    });
+    // fs.writeFileSync("message.txt", "DUMMY");
+    res.writeHead(302, { Location: "/" }); // Chuyển hướng về trang chủ
+    return res.end(); // Dừng hẳn chương trình sau khi phản hồi
+  }
+
+  // Mặc định cho các đường dẫn khác
   // console.log("Request received: " + req.url);
   // res.writeHead(200, { "Content-Type": "text/plain" });
   // res.end("Hello World\n");
